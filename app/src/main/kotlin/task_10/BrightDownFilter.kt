2d64dbd4faefa2107f001ecce317af1fa5475eea
ursuscastus <ursuscastus@gmail.com>
2020-05-21 11:22:40 +0400

    task/10/1 - correction 1

--- app/src/main/kotlin/task_10/BrightDownFilter.kt
+++ app/src/main/kotlin/task_10/BrightDownFilter.kt
@@ -1,37 +1,45 @@
 package task_10
 
 import android.graphics.*
+import android.util.Log
 import kotlinx.coroutines.*
 
-class BrightDownFilter {
+class BrightDownFilter() {
 
-    //launch — метод для асинхронного запуска корутины
-    //Dispatchers.IO используется для фоновых задач, не блокирующих основной поток
+    private val TAG_BRIGHT_DOWN_FILTER: String = "TagBrightDownFilter"
+    private val COLOR_COEFFICIENT: Float = 0.9f //-10% brightness
 
-//    CoroutineScope(Dispatchers.IO).launch {
-//    }
+    lateinit var mBitmap: Bitmap
 
-    //для отложенной задачи
-//    GlobalScope.launch { }
+    suspend fun applyBrightnessFilter(bitmap: Bitmap): Bitmap {
+        mBitmap = Bitmap.createBitmap(bitmap)
+        Log.d(TAG_BRIGHT_DOWN_FILTER, "Method applyBrightnessFilter before withContext - " + Thread.currentThread().name)
+        withContext(Dispatchers.Default) {
+            Log.d(TAG_BRIGHT_DOWN_FILTER, "Method applyBrightnessFilter Context - " + Thread.currentThread().name)
+            Log.d(TAG_BRIGHT_DOWN_FILTER, "applyBrightnessFilter BEFORE - " + mBitmap)
+            mBitmap = changeBrightnessBitmap(mBitmap)
+            Log.d(TAG_BRIGHT_DOWN_FILTER, "applyBrightnessFilter AFTER - " + mBitmap)
+        }
+        return mBitmap
+    }
 
-//    withContext(Dispatchers.IO) {}
+    fun changeBrightnessBitmap(bitmap: Bitmap): Bitmap {
+        Log.d(TAG_BRIGHT_DOWN_FILTER, "Method changeBrightnessBitmap - " + Thread.currentThread().name)
 
-        fun changeBrightnessBitmap(bitmap: Bitmap, colorCoefficient: Float): Bitmap {
+        var changeBrightnessMatrix = floatArrayOf(COLOR_COEFFICIENT, 0f, 0f, 0f, 0f,
+                0f, COLOR_COEFFICIENT, 0f, 0f, 0f,
+                0f, 0f, COLOR_COEFFICIENT, 0f, 0f,
+                0f, 0f, 0f, 1f, 0f)
 
-            var changeBrightnessMatrix = floatArrayOf(colorCoefficient, 0f, 0f, 0f, 0f,
-                    0f, colorCoefficient, 0f, 0f, 0f,
-                    0f, 0f, colorCoefficient, 0f, 0f,
-                    0f, 0f, 0f, 1f, 0f)
-
-            var colorMatrix = ColorMatrix(changeBrightnessMatrix)
-            var width = bitmap.width
-            var height = bitmap.height
-            var bitmapResult = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-            var canvas = Canvas(bitmapResult)
-            var paint = Paint()
-            var colorMatrixColorFilter = ColorMatrixColorFilter(colorMatrix)
-            paint.colorFilter = colorMatrixColorFilter
-            canvas.drawBitmap(bitmap, 0f, 0f, paint)
+        var colorMatrix = ColorMatrix(changeBrightnessMatrix)
+        var width = bitmap.width
+        var height = bitmap.height
+        var bitmapResult = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        var canvas = Canvas(bitmapResult)
+        var paint = Paint()
+        var colorMatrixColorFilter = ColorMatrixColorFilter(colorMatrix)
+        paint.colorFilter = colorMatrixColorFilter
+        canvas.drawBitmap(bitmap, 0f, 0f, paint)
         return bitmapResult
     }
 }

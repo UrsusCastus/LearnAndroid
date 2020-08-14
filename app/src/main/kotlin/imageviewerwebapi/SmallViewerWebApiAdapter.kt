824f1e96f0b962c682bb570f3c2f681de4ba569b
ursuscastus <ursuscastus@gmail.com>
2020-08-14 13:10:35 +0400

    task/15/1 - correction/2

--- app/src/main/kotlin/imageviewerwebapi/SmallViewerWebApiAdapter.kt
+++ app/src/main/kotlin/imageviewerwebapi/SmallViewerWebApiAdapter.kt
@@ -1,13 +1,12 @@
 package imageviewerwebapi
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.application.R
-import com.squareup.picasso.MemoryPolicy
-import com.squareup.picasso.NetworkPolicy
 import com.squareup.picasso.Picasso
 
 class SmallViewerWebApiAdapter(
@@ -18,7 +17,7 @@ class SmallViewerWebApiAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SmallViewerWebApiViewHolder {
         val view: View = LayoutInflater.from(parent.context)
-            .inflate(R.layout.web_api_recycler_view_item_for_small_viewr, parent, false)
+            .inflate(R.layout.web_api_recycler_view_item_for_small_viewer, parent, false)
         return SmallViewerWebApiViewHolder(view)
     }
 
@@ -27,10 +26,9 @@ class SmallViewerWebApiAdapter(
     }
 
     override fun onBindViewHolder(holder: SmallViewerWebApiViewHolder, position: Int) {
+        holder.imageClean()
         Picasso.get()
             .load(itemSmallViewerArrayList[position].url)
-            .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
-            .networkPolicy(NetworkPolicy.NO_CACHE, NetworkPolicy.NO_STORE)
             .placeholder(R.drawable.ic_image_not_download)
             .error(R.drawable.ic_error)
             .fit()
--- app/src/main/kotlin/utils/Utils.kt
+++ app/src/main/kotlin/utils/Utils.kt
@@ -1,6 +1,7 @@
 package utils
 
 import android.content.Context
+import android.content.res.Configuration
 import android.net.*
 
 class Utils {
@@ -8,7 +9,7 @@ class Utils {
     //Используется для обращения к методам и свойствам объекта через имя содержащего его класса
     // без явного указания имени объекта
     companion object {
-        fun isNetworkConnection(context: Context): Boolean {
+        fun isNetworkConnected(context: Context): Boolean {
             //Note: getActiveNetworkInfo() was deprecated in Android 10.
             // Use NetworkCallbacks instead for apps that target Android 10 (API level 29) and higher.
             val connectivityManager: ConnectivityManager =
@@ -16,6 +17,13 @@ class Utils {
             val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
             if (networkInfo != null) {
                 return networkInfo.isConnectedOrConnecting
+            }
+            return false
+        }
+
+        fun isLandScape(context: Context): Boolean {
+            if (context.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                return true
             }
             return false
         }
